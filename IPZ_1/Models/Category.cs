72d196e98e9d61ed89cc008b2458c20677c1bcd4
Raiker1415 <121517060+Raiker1415@users.noreply.github.com>
2023-06-21 14:56:37 +0300

    Add files via upload

---
+++ IPZ_1/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace IPZ_1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Display Order must be greater then 0")]
+        public string DisplayOrder { get; set; }
+
+    }
+}
