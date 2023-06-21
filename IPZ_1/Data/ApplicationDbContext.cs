72a8e32f0d4cec8b15503e48c3964157d721a921
Raiker1415 <121517060+Raiker1415@users.noreply.github.com>
2023-06-21 14:55:16 +0300

    Add files via upload

---
+++ IPZ_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using IPZ_1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPZ_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+    }
+}
