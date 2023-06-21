73645c3667922e2b4ed5368f6e7486da9450833e
Raiker1415 <121517060+Raiker1415@users.noreply.github.com>
2023-06-21 14:55:38 +0300

    Add files via upload

---
+++ IPZ_1/Hubs/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using IPZ_1.Models.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace IPZ_1.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task RefreshProducts(string message)
+        {
+            await Clients.All.SendAsync("RefreshProducts", message);
+        }
+    }
+}
