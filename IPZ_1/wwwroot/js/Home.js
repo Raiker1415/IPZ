19ffcd23031f9f9dcd10e562626ac59cf50a589a
Raiker1415 <121517060+Raiker1415@users.noreply.github.com>
2023-06-21 15:12:01 +0300

    Add files via upload

---
+++ IPZ_1/wwwroot/js/Home.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+
+
+
+$(document).ready(() => {
+
+    var connection = new signalR.HubConnectionBuilder().withUrl("/notification").build();
+
+    connection.on("RefreshProducts", function () {
+        console.log("Hello world!");
+    });
+
+    connection.start();
+});
